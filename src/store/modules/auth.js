9ebb2ced364eef38995c28891ba952ce3f74a1e5
kaltham <terizisays@gmail.com>
2021-05-16 15:02:32 +0400

    adding router and vuex

---
+++ src/store/modules/auth.js
@@ -0,0 +1,31 @@
+const state = {
+    user: null,
+    posts: null,
+};
+
+const getters = {
+    StateUser: state => state.user
+};
+
+const actions = {
+    async Register({dispatch}, form) {
+        
+    },
+    
+    async Login({commit}, User){
+
+    }
+
+};
+
+const mutations = {
+
+}
+
+export default{
+
+    state,
+    getters,
+    actions,
+    mutations
+};
---
+++ src/views/Posts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="posts">
+      <div v-if="User">
+        <p>Hi {{User}}</p>
+      </div>
+   </div>
+</template>
+
+<script>
+import { mapGetters} from "vuex";
+export default{
+    name: 'Posts Page',
+    components: {
+        
+    },
+    computed:{
+        ...mapGetters({User: "StateUser"})
+    }
+};
+
+</script>
